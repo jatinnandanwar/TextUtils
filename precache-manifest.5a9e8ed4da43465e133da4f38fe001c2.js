c36c0fee4677d94b16fa1f541ce168245206f0ef
jatinnandanwar <92711557+jatinnandanwar@users.noreply.github.com>
2022-11-21 20:21:12 +0530

    Updates

---
+++ precache-manifest.5a9e8ed4da43465e133da4f38fe001c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9be035ac0b5ca863c35b",
+    "url": "/jatinnandanwar/TextUtils/static/css/main.ffde95b4.chunk.css"
+  },
+  {
+    "revision": "9be035ac0b5ca863c35b",
+    "url": "/jatinnandanwar/TextUtils/static/js/main.9be035ac.chunk.js"
+  },
+  {
+    "revision": "bfaef02fc73415496900",
+    "url": "/jatinnandanwar/TextUtils/static/js/1.bfaef02f.chunk.js"
+  },
+  {
+    "revision": "e80c7fae8b14ae8bf834",
+    "url": "/jatinnandanwar/TextUtils/static/js/2.e80c7fae.chunk.js"
+  },
+  {
+    "revision": "309dfc946e061e68e73f",
+    "url": "/jatinnandanwar/TextUtils/static/js/runtime~main.309dfc94.js"
+  },
+  {
+    "revision": "19f820434edd477db35e2c52a829dfea",
+    "url": "/jatinnandanwar/TextUtils/index.html"
+  }
+];
