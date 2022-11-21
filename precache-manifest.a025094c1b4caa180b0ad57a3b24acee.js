8c6ba877daa265fdd5ef9122775bfba5eda77888
jatinnandanwar <92711557+jatinnandanwar@users.noreply.github.com>
2022-11-21 21:01:52 +0530

    Updates

---
+++ precache-manifest.a025094c1b4caa180b0ad57a3b24acee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9be035ac0b5ca863c35b",
+    "url": "/TextUtils/static/css/main.ffde95b4.chunk.css"
+  },
+  {
+    "revision": "9be035ac0b5ca863c35b",
+    "url": "/TextUtils/static/js/main.9be035ac.chunk.js"
+  },
+  {
+    "revision": "bfaef02fc73415496900",
+    "url": "/TextUtils/static/js/1.bfaef02f.chunk.js"
+  },
+  {
+    "revision": "e80c7fae8b14ae8bf834",
+    "url": "/TextUtils/static/js/2.e80c7fae.chunk.js"
+  },
+  {
+    "revision": "4cb78b987c006adae77e",
+    "url": "/TextUtils/static/js/runtime~main.4cb78b98.js"
+  },
+  {
+    "revision": "23fbd0e90cc17b5f3d72a23b5ef8f4ee",
+    "url": "/TextUtils/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/jatinnandanwar/TextUtils/precache-manifest.5a9e8ed4da43465e133da4f38fe001c2.js"
+  "/TextUtils/precache-manifest.a025094c1b4caa180b0ad57a3b24acee.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/jatinnandanwar/TextUtils/index.html", {
+workbox.routing.registerNavigationRoute("/TextUtils/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
